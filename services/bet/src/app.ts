ed88cf5239cf99c58e158bfd13f870ba60c8b3e8
João Pinho <jpe.pinho@gmail.com>
2021-03-13 21:15:45 +0000

    touch

--- services/bet/src/app.ts
+++ services/bet/src/app.ts
@@ -7,7 +7,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use((req, __, next) => {
-  console.log(`Request received | ${req.method} ${req.path} ${req.body || ''}`);
+  console.log(`Request received || ${req.method} ${req.path} ${req.body || ''}`);
   next();
 });
 
